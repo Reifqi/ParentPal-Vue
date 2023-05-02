5d8cf6438d7e7c68a06ea76090c747f90a08b42e
Reifqi <rifqi_m20@mhs.unsyiah.ac.id>
2023-05-02 08:46:40 +0700

    Update community vue(final)

--- src/components/pages/Community.vue
+++ src/components/pages/Community.vue
@@ -20,10 +20,38 @@
                     </a>
                 </div>
                 <div class="card">
-                    <a href="https://t.me/+jxOTkR8lf5E4ODM9">
-                    <img src="../../assets/image/com-mompreneurs.svg" alt="logofeature"/>
-                    <h3>Mompreneurs</h3>
-                    <p>Join this community if you are a mompreneurs!</p>
+                    <a href="https://t.me/+E4VUSoSjFf9kYWQ1">
+                    <img src="../../assets/image/com-pregnant.svg" alt="logofeature"/>
+                    <h3>Pregnant</h3>
+                    <p>Join this community if you are a pregnant mom!</p>
+                    </a>
+                </div>
+                <div class="card">
+                    <a href="https://t.me/+o6v48OXTQSA3Yjhl">
+                    <img src="../../assets/image/com-singlemom.svg" alt="logofeature"/>
+                    <h3>Single Mom</h3>
+                    <p>Join this community if you are a single mom!</p>
+                    </a>
+                </div>
+                <div class="card">
+                    <a href="https://t.me/+bNSwjGdh2MZhMDBl">
+                    <img src="../../assets/image/com-workingmom.svg" alt="logofeature"/>
+                    <h3>Working Mom</h3>
+                    <p>Join this community if you are a working mom!</p>
+                    </a>
+                </div>
+                <div class="card">
+                    <a href="https://t.me/+xR1k13RFI7w1MjFl">
+                    <img src="../../assets/image/com-specialkids.svg" alt="logofeature"/>
+                    <h3>Special kids</h3>
+                    <p>Join this community if you have special kids!</p>
+                    </a>
+                </div>
+                <div class="card">
+                    <a href="https://t.me/+asZ_jDOsxIU5OTll">
+                    <img src="../../assets/image/com-specialkids.svg" alt="logofeature"/>
+                    <h3>Newborn</h3>
+                    <p>Join this community if you are a working mom!</p>
                     </a>
                 </div>
             </div>
